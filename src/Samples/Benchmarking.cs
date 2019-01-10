3cf9fb7f7622803f97f406cb3ce8f224eb4a5d93
Daniel Holstein <daholste@microsoft.com>
2019-01-10 15:13:45 -0800

    rev benchmarking sample script

--- src/Samples/Benchmarking.cs
+++ src/Samples/Benchmarking.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.ML;
+﻿using System;
+using Microsoft.ML;
 using Microsoft.ML.Auto;
 
 namespace Samples
 {
     public static class Benchmarking
     {
-        const string TrainDataPath = @"C:\data\sample_train2.csv";
+        const string TrainDataPath = @"D:\SplitDatasets\EverGreen_train.csv";
+        const string ValidationDataPath = @"D:\SplitDatasets\EverGreen_valid.csv";
+        const string TestDataPath = @"D:\SplitDatasets\EverGreen_test.csv";
         const string Label = "Label";
 
         public static void Run()
@@ -13,8 +16,23 @@ namespace Samples
             var context = new MLContext();
             var columnInference = context.Data.InferColumns(TrainDataPath, Label, true);
             var textLoader = context.Data.CreateTextReader(columnInference);
-            var data = textLoader.Read(TrainDataPath);
-            var pipeline = context.BinaryClassification.AutoFit(data, Label);
+            var trainData = textLoader.Read(TrainDataPath);
+            var validationData = textLoader.Read(ValidationDataPath);
+            var testData = textLoader.Read(TestDataPath);
+            var best = context.BinaryClassification.AutoFit(trainData, Label, validationData, settings:
+                new AutoFitSettings()
+                {
+                    StoppingCriteria = new ExperimentStoppingCriteria()
+                    {
+                        MaxIterations = 200,
+                        TimeOutInMinutes = 1000000000
+                    }
+                });
+            var scoredTestData = best.BestPipeline.Model.Transform(testData);
+            var testDataMetrics = context.BinaryClassification.EvaluateNonCalibrated(scoredTestData);
+
+            Console.WriteLine(testDataMetrics.Accuracy);
+            Console.ReadLine();
         }
     }
 }
